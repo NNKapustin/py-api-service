c44b0a998f918007f9d88a1b88e10fec58241179
nikita <nikita.n.kapustin@gmail.com>
2023-09-23 16:46:46 +0300

    Create separate views for users, shops and partners. Delete partner update -will be included in partner viewset

--- orders/backend/urls.py
+++ orders/backend/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from backend.views import PartnerUpdate
 from rest_framework.routers import DefaultRouter
-from backend.views import UserViewSet, AddressViewSet
+from .views import UserViewSet, AddressViewSet
 from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm
 
 router = DefaultRouter()
@@ -12,6 +11,4 @@ app_name = 'backend'
 urlpatterns = [
     path('user/password_reset/', reset_password_request_token, name='password-reset'),
     path('user/password_reset/confirm/', reset_password_confirm, name='password-reset-confirm'),
-
-    path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
 ] + router.urls
